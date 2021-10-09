765588a80428c4aed794ba27755c58754b73a6a1
CalinGirban <92202429+CalinGirban@users.noreply.github.com>
2021-10-09 12:09:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+from turtle import *
+p=getscreen()
+penup()
+setpos(-100,-250)
+pendown()
+pensize(4)
+for x in range(4):
+    if x%2==0:
+        forward(250)
+    else:
+        forward(50)
+    left(90)
+pensize(2)
+penup()
+setpos(-100,-233)
+pendown()
+forward(250)
+penup()
+setpos(-100,-217)
+pendown()
+forward(250)
+penup()
